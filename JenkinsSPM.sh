6ec9a02b97017d262f7f940c59be3c9d18b72bfe
Robbert Brandsma <i@robbertbrandsma.nl>
2016-04-13 13:07:16 +0200

    Update JenkinsSPM.sh

--- JenkinsSPM.sh
+++ JenkinsSPM.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -ex
 
+# Run this script by adding a jenkins build script with the following contents:
+# #!/bin/bash -ex
+# bash <(curl -s https://raw.githubusercontent.com/PlanTeam/Tools/master/JenkinsSPM.sh)
+
 echo "Build script running as user $(whoami) in directory $(pwd)"
+
+if [ -e "~/.bashrc"]; then
+	~/.bashrc
+fi
+
+if [ -e "~/.bash_profile"]; then
+	~/.bash_profile
+fi
 
 if [ -e "/usr/local/bin/swiftenv" ]; then
 	export PATH="/usr/local/bin:$PATH"
